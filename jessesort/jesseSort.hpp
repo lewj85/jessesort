11a31fd79f18ff71f017a32ea3a531173b06b602
Jesse Lew <lewj85@gmail.com>
2025-02-18 23:58:05 +0000

    Added jessesort C++ implementation

---
+++ jessesort/jesseSort.hpp
@@ -0,0 +1,8 @@
+#ifndef JESSESORT_HPP  // Include guard
+#define JESSESORT_HPP
+
+#include <vector>
+
+std::vector<int> jesseSort(std::vector<int>& arr);
+
+#endif // JESSESORT_HPP
---
+++ jessesort/main.cpp
@@ -0,0 +1,30 @@
+#include "jesseSort.hpp"
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    // Set a seed for random number generation
+    std::srand(std::time(nullptr));
+
+    // Define the size of the vector you want to test
+    size_t vectorSize = 10;
+
+    // Create a random vector of integers
+    std::vector<int> arr(vectorSize);
+    for (size_t i = 0; i < vectorSize; ++i) {
+        // Random values between 0 and vectorSize-1
+        arr[i] = std::rand() % vectorSize;
+        // vs std::uniform_int_distribution
+    }
+
+    std::vector<int> sortedArr = jesseSort(arr);
+    //std::vector<int> sortedArr = patienceSortAscendingPiles(arr);
+
+    std::cout << "Sorted Array: ";
+    for (int x : sortedArr) std::cout << x << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
